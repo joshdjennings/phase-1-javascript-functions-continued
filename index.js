09476bcbd9ba647139d059085f4f45047b82f98f
joshua david jennings <jennings.josh@gmail.com>
2021-08-25 17:00:45 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(workActivity = "go to the office"){
+    return `This Monday, I will ${workActivity}.`;
+
+}
+
+function wrapAdjective(highlight = "*"){
+    return function(statement){
+       return `You are ${highlight}${statement}${highlight}!`;
+       
+    }
+    
+}
