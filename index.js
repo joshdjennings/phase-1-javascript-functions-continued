bca03af5a1fe420b2e00c88eb81b93c44b86bba4
joshua david jennings <jennings.josh@gmail.com>
2021-08-26 17:48:14 -0500

    commit tree ed2820c7db58271631e8c7834bf44efd625dca04

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function saturdayFun(activity = "roller-skate"){
-    return `This Saturday, I want to ${activity}!`;
-}
+ function saturdayFun(activity = "roller-skate"){
+     return `This Saturday, I want to ${activity}!`;
+ }
+saturdayFun("bathe my dog")
+// function mondayWork(workActivity = "go to the office"){
+//     return `This Monday, I will ${workActivity}.`;
 
-function mondayWork(workActivity = "go to the office"){
-    return `This Monday, I will ${workActivity}.`;
+// }
 
-}
-
-function wrapAdjective(highlight = "*"){
-    return function(statement){
-       return `You are ${highlight}${statement}${highlight}!`;
-       
-    }
+// function wrapAdjective(highlight = "*"){
+//     return function(statement){
+//        return `You are ${highlight}${statement}${highlight}!`;
+     
+//     }
     
-}
+// }
